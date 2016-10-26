fe90af669547f39bc4350149a343c960f02d145b
DanTheGeekMan <danthegeekman@googlemail.com>
2016-10-26 17:30:22 +0100

    Added update to user factory.

--- src/js/factories/user.js
+++ src/js/factories/user.js
@@ -6,6 +6,7 @@ userFactory.$inject =["API", "$resource"];
 function userFactory(API, $resource){
   return $resource(`${API}/users/:id`, { id: "@_id"}, {
     'query':      { method: "GET", isArray: false },
+    'update':     { method: "PUT" },
     'register':   { method: "POST", url: `${API}/register`},
     'login':      { method: "POST", url: `${API}/login`}
   });
